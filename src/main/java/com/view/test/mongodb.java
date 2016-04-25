18e75a7004bc46926d56c9616de0c8939db6a7a4
ShineJaie <shinejaie@gmail.com>
2016-04-25 22:42:48 +0800

    将 MongoDB 与 Java 整合应用, 实现简单的添加测试; pom 文件配置 Maven 中央仓库

---
+++ src/main/java/com/view/test/mongodb.java
@@ -0,0 +1,29 @@
+package com.view.test;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.Calendar;
+
+/**
+ * MongoDB 测试类
+ * Created by admin on 2016/4/25.
+ */
+public class mongodb {
+
+    public static void main(String[] args) {
+
+        // Connect to a MongoDB instance running on the localhost on the default port 27017
+        MongoClient mongoClient = new MongoClient();
+
+        // Once successfully connected, access the test database
+        MongoDatabase db = mongoClient.getDatabase("test");
+
+        Document document = new Document();
+        document.put("name", "吴仙杰");
+        document.put("time", Calendar.getInstance().getTime());
+        db.getCollection("insertTest").insertOne(document);
+    }
+
+}
